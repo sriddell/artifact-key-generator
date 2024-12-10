7b1efd61ac1abb93456e487e25bd231f0cd51129
Shane Riddell <shane.riddell@ellucian.com>
2024-12-10 13:52:40 -0500

    Fix output

--- main.go
+++ main.go
@@ -10,29 +10,27 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: artifact-key-generator <filename>")
-		return
+		os.Exit(1)
 	}
 
 	filename := os.Args[1]
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Printf("Error opening file: %v\n", err)
-		return
+		os.Exit(1)
 	}
 	defer file.Close()
 
 	hash := sha512.New()
 	if _, err := io.Copy(hash, file); err != nil {
 		fmt.Printf("Error reading file: %v\n", err)
-		return
+		os.Exit(1)
 	}
 	fileInfo, err := file.Stat()
 	if err != nil {
 		fmt.Printf("Error getting file info: %v\n", err)
-		return
+		os.Exit(1)
 	}
-	fmt.Printf("Size of %s: %d bytes\n", filename, fileInfo.Size())
-
 	hashString := fmt.Sprintf("%x", hash.Sum(nil))
 	fileSize := fileInfo.Size()
 	fmt.Printf("%s-%d\n", hashString, fileSize)
